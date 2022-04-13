5c63dc7bce46741397568c9055d6f7588843c7e7
MrBBot <me@mrbbot.dev>
2022-04-13 13:17:15 +0100

    `class::descriptors` module tests and docs

--- src/class/descriptors/parser.rs
+++ src/class/descriptors/parser.rs
@@ -1,3 +1,27 @@
+//! Parsers for field and method descriptors representing field types and method parameter/return
+//! types respectively.
+//!
+//! Parsers are implemented using parser combinators, higher-order functions composing smaller
+//! parsers into larger ones. Each combinator returns an output and the remaining input, or an error
+//! if parsing failed. The [`nom`] crate defines a selection of [combinators].
+//!
+//! [combinators]: https://github.com/Geal/nom/blob/main/doc/choosing_a_combinator.md
+//!
+//! # Grammar
+//!
+//! Adapted from sections [4.3.2] and [4.3.3] of the Java Virtual Machine Specification:
+//!
+//! ```text
+//! FieldDescriptor  ::= BaseType | ObjectType | ArrayType
+//! BaseType         ::= 'B' | 'C' | 'D' | 'F' | 'I' | 'J' | 'S' | 'Z'
+//! ObjectType       ::= 'L' ClassName ';'
+//! ArrayType        ::= '[' FieldDescriptor
+//! MethodDescriptor ::= '(' FieldDescriptor* ')' ReturnDescriptor
+//! ReturnDescriptor ::= FieldDescriptor | 'V'
+//! ```
+//!
+//! [4.3.2]: https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.3.2
+//! [4.3.3]: https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.3.3
 use crate::class::{FieldDescriptor, MethodDescriptor, ReturnDescriptor};
 use nom::branch::alt;
 use nom::bytes::complete::take_until1;
@@ -7,9 +31,10 @@ use nom::multi::many0;
 use nom::sequence::tuple;
 use nom::IResult;
 
-// https://github.com/Geal/nom/blob/main/doc/choosing_a_combinator.md
-
-// https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.3.2
+/// Parses a field descriptor according to the grammar defined in section [4.3.2] of the Java
+/// Virtual Machine Specification.
+///
+/// [4.3.2]: https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.3.2
 pub fn field_descriptor_parser(input: &str) -> IResult<&str, FieldDescriptor> {
     alt((
         value(FieldDescriptor::Byte, char('B')),
@@ -31,7 +56,10 @@ pub fn field_descriptor_parser(input: &str) -> IResult<&str, FieldDescriptor> {
     ))(input)
 }
 
-// https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.3.3
+/// Parses a return descriptor according to the grammar defined in section [4.3.3] of the Java
+/// Virtual Machine Specification.
+///
+/// [4.3.3]: https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.3.3
 pub fn return_descriptor_parser(input: &str) -> IResult<&str, ReturnDescriptor> {
     alt((
         value(ReturnDescriptor::Void, char('V')),
@@ -41,7 +69,10 @@ pub fn return_descriptor_parser(input: &str) -> IResult<&str, ReturnDescriptor> 
     ))(input)
 }
 
-// https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.3.3
+/// Parses a method descriptor according to the grammar defined in section [4.3.3] of the Java
+/// Virtual Machine Specification.
+///
+/// [4.3.3]: https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.3.3
 pub fn method_descriptor_parser(input: &str) -> IResult<&str, MethodDescriptor> {
     map(
         tuple((
@@ -59,122 +90,127 @@ mod tests {
     use crate::class::*;
 
     #[test]
-    fn field_descriptor_parser_parses_base_types() {
-        assert_eq!(
-            field_descriptor_parser("B"),
-            Ok(("", FieldDescriptor::Byte))
-        );
-        assert_eq!(
-            field_descriptor_parser("C"),
-            Ok(("", FieldDescriptor::Char))
-        );
-        assert_eq!(
-            field_descriptor_parser("D"),
-            Ok(("", FieldDescriptor::Double))
-        );
-        assert_eq!(
-            field_descriptor_parser("F"),
-            Ok(("", FieldDescriptor::Float))
-        );
-        assert_eq!(field_descriptor_parser("I"), Ok(("", FieldDescriptor::Int)));
-        assert_eq!(
-            field_descriptor_parser("J"),
-            Ok(("", FieldDescriptor::Long))
-        );
-        assert_eq!(
-            field_descriptor_parser("S"),
-            Ok(("", FieldDescriptor::Short))
-        );
-        assert_eq!(
-            field_descriptor_parser("Z"),
-            Ok(("", FieldDescriptor::Boolean))
-        );
+    fn field_descriptor_parser_parses_base_types() -> anyhow::Result<()> {
+        let (rest, d) = field_descriptor_parser("B")?;
+        assert_eq!(format!("{}", d), "B");
+        assert_eq!((rest, d), ("", FieldDescriptor::Byte));
+
+        let (rest, d) = field_descriptor_parser("C")?;
+        assert_eq!(format!("{}", d), "C");
+        assert_eq!((rest, d), ("", FieldDescriptor::Char));
+
+        let (rest, d) = field_descriptor_parser("D")?;
+        assert_eq!(format!("{}", d), "D");
+        assert_eq!((rest, d), ("", FieldDescriptor::Double));
+
+        let (rest, d) = field_descriptor_parser("F")?;
+        assert_eq!(format!("{}", d), "F");
+        assert_eq!((rest, d), ("", FieldDescriptor::Float));
+
+        let (rest, d) = field_descriptor_parser("I")?;
+        assert_eq!(format!("{}", d), "I");
+        assert_eq!((rest, d), ("", FieldDescriptor::Int));
+
+        let (rest, d) = field_descriptor_parser("J")?;
+        assert_eq!(format!("{}", d), "J");
+        assert_eq!((rest, d), ("", FieldDescriptor::Long));
+
+        let (rest, d) = field_descriptor_parser("S")?;
+        assert_eq!(format!("{}", d), "S");
+        assert_eq!((rest, d), ("", FieldDescriptor::Short));
+
+        let (rest, d) = field_descriptor_parser("Z")?;
+        assert_eq!(format!("{}", d), "Z");
+        assert_eq!((rest, d), ("", FieldDescriptor::Boolean));
+
+        Ok(())
     }
 
     #[test]
-    fn field_descriptor_parser_parses_objects() {
-        assert_eq!(
-            field_descriptor_parser("Ljava/lang/Thread;"),
-            Ok((
-                "",
-                FieldDescriptor::Object(String::from("java/lang/Thread"))
-            ))
-        );
-        assert_eq!(
-            field_descriptor_parser("Ljava/lang/Object;"),
-            Ok((
-                "",
-                FieldDescriptor::Object(String::from("java/lang/Object"))
-            ))
-        );
+    fn field_descriptor_parser_parses_objects() -> anyhow::Result<()> {
+        let (rest, d) = field_descriptor_parser("Ljava/lang/Thread;")?;
+        assert_eq!(format!("{}", d), "Ljava/lang/Thread;");
+        assert_eq!(rest, "");
+        assert_eq!(d, FieldDescriptor::Object(String::from("java/lang/Thread")));
+
+        let (rest, d) = field_descriptor_parser("Ljava/lang/Object;")?;
+        assert_eq!(format!("{}", d), "Ljava/lang/Object;");
+        assert_eq!(rest, "");
+        assert_eq!(d, FieldDescriptor::Object(String::from("java/lang/Object")));
+
+        Ok(())
     }
 
     #[test]
-    fn field_descriptor_parser_parses_arrays() {
+    fn field_descriptor_parser_parses_arrays() -> anyhow::Result<()> {
+        let (rest, d) = field_descriptor_parser("[D")?;
+        assert_eq!(format!("{}", d), "[D");
+        assert_eq!(rest, "");
+        assert_eq!(d, FieldDescriptor::Array(Box::new(FieldDescriptor::Double)));
+
+        let (rest, d) = field_descriptor_parser("[[[Ljava/lang/Object;")?;
+        assert_eq!(format!("{}", d), "[[[Ljava/lang/Object;");
+        assert_eq!(rest, "");
         assert_eq!(
-            field_descriptor_parser("[D"),
-            Ok((
-                "",
-                FieldDescriptor::Array(Box::new(FieldDescriptor::Double))
-            ))
-        );
-        assert_eq!(
-            field_descriptor_parser("[[[Ljava/lang/Object;"),
-            Ok((
-                "",
-                FieldDescriptor::Array(Box::new(FieldDescriptor::Array(Box::new(
-                    FieldDescriptor::Array(Box::new(FieldDescriptor::Object(String::from(
-                        "java/lang/Object"
-                    ))))
+            d,
+            FieldDescriptor::Array(Box::new(FieldDescriptor::Array(Box::new(
+                FieldDescriptor::Array(Box::new(FieldDescriptor::Object(String::from(
+                    "java/lang/Object"
                 ))))
-            ))
+            ))))
         );
+
+        Ok(())
     }
 
     #[test]
-    fn return_descriptor_parser_parses_void() {
-        assert_eq!(
-            return_descriptor_parser("V"),
-            Ok(("", ReturnDescriptor::Void))
-        );
+    fn return_descriptor_parser_parses_void() -> anyhow::Result<()> {
+        let (rest, d) = return_descriptor_parser("V")?;
+        assert_eq!(format!("{}", d), "V");
+        assert_eq!((rest, d), ("", ReturnDescriptor::Void));
+        Ok(())
     }
 
     #[test]
-    fn return_descriptor_parser_parses_fields() {
+    fn return_descriptor_parser_parses_fields() -> anyhow::Result<()> {
+        let (rest, d) = return_descriptor_parser("B")?;
+        assert_eq!(format!("{}", d), "B");
+        assert_eq!(rest, "");
+        assert_eq!(d, ReturnDescriptor::Field(FieldDescriptor::Byte));
+
+        let (rest, d) = return_descriptor_parser("Ljava/lang/Object;")?;
+        assert_eq!(format!("{}", d), "Ljava/lang/Object;");
+        assert_eq!(rest, "");
         assert_eq!(
-            return_descriptor_parser("B"),
-            Ok(("", ReturnDescriptor::Field(FieldDescriptor::Byte)))
+            d,
+            ReturnDescriptor::Field(FieldDescriptor::Object(String::from("java/lang/Object")))
         );
+
+        Ok(())
+    }
+
+    #[test]
+    fn method_descriptor_parser_parses_methods() -> anyhow::Result<()> {
+        let (rest, d) = method_descriptor_parser("(IDLjava/lang/Thread;)Ljava/lang/Object;")?;
+        assert_eq!(format!("{}", d), "(IDLjava/lang/Thread;)Ljava/lang/Object;");
+        assert_eq!(rest, "");
         assert_eq!(
-            return_descriptor_parser("Ljava/lang/Object;"),
-            Ok((
-                "",
+            d,
+            MethodDescriptor::new(
+                vec![
+                    FieldDescriptor::Int,
+                    FieldDescriptor::Double,
+                    FieldDescriptor::Object(String::from("java/lang/Thread"))
+                ],
                 ReturnDescriptor::Field(FieldDescriptor::Object(String::from("java/lang/Object")))
-            ))
+            )
         );
-    }
 
-    #[test]
-    fn method_descriptor_parser_parses_methods() {
-        assert_eq!(
-            method_descriptor_parser("(IDLjava/lang/Thread;)Ljava/lang/Object;",),
-            Ok((
-                "",
-                MethodDescriptor::new(
-                    vec![
-                        FieldDescriptor::Int,
-                        FieldDescriptor::Double,
-                        FieldDescriptor::Object(String::from("java/lang/Thread"))
-                    ],
-                    ReturnDescriptor::Field(FieldDescriptor::Object(String::from(
-                        "java/lang/Object"
-                    )))
-                )
-            ))
-        );
-        assert_eq!(
-            method_descriptor_parser("()V"),
-            Ok(("", MethodDescriptor::new(vec![], ReturnDescriptor::Void)))
-        );
+        let (rest, d) = method_descriptor_parser("()V")?;
+        assert_eq!(format!("{}", d), "()V");
+        assert_eq!(rest, "");
+        assert_eq!(d, MethodDescriptor::new(vec![], ReturnDescriptor::Void));
+
+        Ok(())
     }
 }
--- src/class/descriptors/types.rs
+++ src/class/descriptors/types.rs
@@ -5,7 +5,17 @@ use std::fmt::Write;
 use std::sync::Arc;
 use wasm_encoder::ValType;
 
-// https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.3.2
+/// Parsed field descriptor representing the type of a field as defined in section [4.3.2] of the
+/// Java Virtual Machine Specification.
+///
+/// See [`field_descriptor_parser`] for the parser implementation.
+///
+/// Derives [`Ord`] so methods can be sorted for deterministic output. Note Rust requires the size
+/// of `enum`s to be known at compile time, so we must add indirection (`Box`) to [`Array`].
+///
+/// [4.3.2]: https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.3.2
+/// [`field_descriptor_parser`]: super::parser::field_descriptor_parser
+/// [`Array`]: FieldDescriptor::Array
 #[derive(Clone, Eq, PartialEq, Hash, Ord, PartialOrd)]
 pub enum FieldDescriptor {
     Byte,                        // B
@@ -44,106 +54,6 @@ impl fmt::Debug for FieldDescriptor {
     }
 }
 
-// https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.3.3
-#[derive(Clone, Eq, PartialEq, Hash, Ord, PartialOrd)]
-pub enum ReturnDescriptor {
-    Void, // V
-    Field(FieldDescriptor),
-}
-
-impl fmt::Display for ReturnDescriptor {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            ReturnDescriptor::Void => f.write_char('V'),
-            ReturnDescriptor::Field(field_type) => fmt::Display::fmt(field_type, f),
-        }
-    }
-}
-
-impl fmt::Debug for ReturnDescriptor {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "ReturnDescriptor {{ {} }}", self)
-    }
-}
-
-#[derive(Debug, Clone, Eq, PartialEq, Hash)]
-pub struct FunctionType {
-    pub params: Vec<ValType>,
-    pub results: Vec<ValType>,
-}
-
-impl FunctionType {
-    pub fn with_implicit_this(&self) -> Self {
-        let mut func_type = self.clone();
-        func_type.params.insert(0, ValType::I32);
-        func_type
-    }
-
-    pub fn dispatcher_name(&self) -> String {
-        let param_names = self.params.iter().copied().map(val_type_name).format("");
-        let result_names = self.results.iter().copied().map(val_type_name).format("");
-        format!("!Dispatcher_{}_{}", param_names, result_names)
-    }
-}
-
-// https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.3.3
-#[derive(Clone, Eq, PartialEq, Hash)]
-pub struct MethodDescriptor {
-    pub params: Vec<FieldDescriptor>,
-    pub returns: ReturnDescriptor,
-    pub function_type: Arc<FunctionType>,
-}
-
-impl PartialOrd<Self> for MethodDescriptor {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for MethodDescriptor {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.params
-            .cmp(&other.params)
-            .then_with(|| self.returns.cmp(&other.returns))
-    }
-}
-
-impl MethodDescriptor {
-    pub fn new(params: Vec<FieldDescriptor>, returns: ReturnDescriptor) -> Self {
-        let function_type = {
-            let params = params.iter().map(FieldDescriptor::as_type).collect();
-            let results = match &returns {
-                ReturnDescriptor::Void => vec![],
-                ReturnDescriptor::Field(field) => vec![field.as_type()],
-            };
-            Arc::new(FunctionType { params, results })
-        };
-        Self {
-            params,
-            returns,
-            function_type,
-        }
-    }
-}
-
-impl fmt::Display for MethodDescriptor {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_char('(')?;
-        for param in &self.params {
-            fmt::Display::fmt(param, f)?;
-        }
-        f.write_char(')')?;
-        fmt::Display::fmt(&self.returns, f)?;
-        Ok(())
-    }
-}
-
-impl fmt::Debug for MethodDescriptor {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "MethodDescriptor {{ {} }}", self)
-    }
-}
-
 impl FieldDescriptor {
     pub fn as_type(&self) -> ValType {
         match self {
@@ -170,6 +80,63 @@ impl FieldDescriptor {
     }
 }
 
+/// Parsed return descriptor representing the return type of a method as defined in section [4.3.3]
+/// of the Java Virtual Machine Specification.
+///
+/// See [`return_descriptor_parser`] for the parser implementation.
+///
+/// Derives [`Ord`] so methods can be sorted for deterministic output.
+///
+/// [4.3.3]: https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.3.3
+/// [`return_descriptor_parser`]: super::parser::return_descriptor_parser
+#[derive(Clone, Eq, PartialEq, Hash, Ord, PartialOrd)]
+pub enum ReturnDescriptor {
+    Void, // V
+    Field(FieldDescriptor),
+}
+
+impl fmt::Display for ReturnDescriptor {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ReturnDescriptor::Void => f.write_char('V'),
+            ReturnDescriptor::Field(field_type) => fmt::Display::fmt(field_type, f),
+        }
+    }
+}
+
+impl fmt::Debug for ReturnDescriptor {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "ReturnDescriptor {{ {} }}", self)
+    }
+}
+
+/// WebAssembly function type corresponding to a parsed [`MethodDescriptor`].
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct FunctionType {
+    pub params: Vec<ValType>,
+    pub results: Vec<ValType>,
+}
+
+impl FunctionType {
+    /// Returns a copy of the `FunctionType` with an additional `i32` parameter at the start for the
+    /// implicit `this` pointer in instance methods.
+    pub fn with_implicit_this(&self) -> Self {
+        let mut func_type = self.clone();
+        func_type.params.insert(0, ValType::I32);
+        func_type
+    }
+
+    /// Returns the name of the built-in dispatcher function for these parameter and return types.
+    pub fn dispatcher_name(&self) -> String {
+        let param_names = self.params.iter().copied().map(val_type_name).format("");
+        let result_names = self.results.iter().copied().map(val_type_name).format("");
+        // The WebAssembly text format does not support '(' and ')' characters in function names
+        // without extra annotations, so use '_'s instead
+        format!("!Dispatcher_{}_{}", param_names, result_names)
+    }
+}
+
+/// Returns the WebAssembly text format's representation of a value type.
 fn val_type_name(t: ValType) -> &'static str {
     match t {
         ValType::I32 => "i32",
@@ -179,5 +146,212 @@ fn val_type_name(t: ValType) -> &'static str {
         ValType::V128 => "v128",
         ValType::FuncRef => "funcref",
         ValType::ExternRef => "externref",
+    }
+}
+
+/// Parsed method descriptor representing the parameter and return types of a method as defined in
+/// section [4.3.3] of the Java Virtual Machine Specification.
+///
+/// See [`method_descriptor_parser`] for the parser implementation.
+///
+/// Implements [`Ord`] so methods can be sorted for deterministic output.
+///
+/// [4.3.3]: https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.3.3
+/// [`method_descriptor_parser`]: super::parser::method_descriptor_parser
+#[derive(Clone, Eq, PartialEq, Hash)]
+pub struct MethodDescriptor {
+    pub params: Vec<FieldDescriptor>,
+    pub returns: ReturnDescriptor,
+
+    /// WebAssembly function type corresponding to this descriptor. Computed at construction and
+    /// reference counted to avoid doing this multiple times later on.
+    pub function_type: Arc<FunctionType>,
+}
+
+impl fmt::Display for MethodDescriptor {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_char('(')?;
+        for param in &self.params {
+            fmt::Display::fmt(param, f)?;
+        }
+        f.write_char(')')?;
+        fmt::Display::fmt(&self.returns, f)?;
+        Ok(())
+    }
+}
+
+impl fmt::Debug for MethodDescriptor {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "MethodDescriptor {{ {} }}", self)
+    }
+}
+
+impl PartialOrd<Self> for MethodDescriptor {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for MethodDescriptor {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.params
+            .cmp(&other.params)
+            .then_with(|| self.returns.cmp(&other.returns))
+    }
+}
+
+impl MethodDescriptor {
+    /// Constructs a new `MethodDescriptor` from parameter and return types.
+    ///
+    /// Also computes the WebAssembly function type corresponding to this descriptor to avoid doing
+    /// this multiple times later on. It's likely we'll need this multiple times for every method
+    /// descriptor we constructor.
+    pub fn new(params: Vec<FieldDescriptor>, returns: ReturnDescriptor) -> Self {
+        // Compute WebAssembly function type
+        let function_type = {
+            let params = params.iter().map(FieldDescriptor::as_type).collect();
+            let results = match &returns {
+                ReturnDescriptor::Void => vec![],
+                ReturnDescriptor::Field(field) => vec![field.as_type()],
+            };
+            Arc::new(FunctionType { params, results })
+        };
+
+        Self {
+            params,
+            returns,
+            function_type,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::class::{FieldDescriptor, FunctionType, MethodDescriptor, ReturnDescriptor};
+    use std::cmp::Ordering;
+    use wasm_encoder::ValType;
+
+    // Descriptor `Display` implementations are tested in the `super::parser::tests` module
+
+    #[test]
+    fn field_descriptor_as_type() {
+        // Integer types
+        assert_eq!(FieldDescriptor::Boolean.as_type(), ValType::I32);
+        assert_eq!(FieldDescriptor::Byte.as_type(), ValType::I32);
+        assert_eq!(FieldDescriptor::Char.as_type(), ValType::I32);
+        assert_eq!(FieldDescriptor::Short.as_type(), ValType::I32);
+        assert_eq!(FieldDescriptor::Int.as_type(), ValType::I32);
+        assert_eq!(FieldDescriptor::Long.as_type(), ValType::I64);
+
+        // Floating point types
+        assert_eq!(FieldDescriptor::Float.as_type(), ValType::F32);
+        assert_eq!(FieldDescriptor::Double.as_type(), ValType::F64);
+
+        // Reference types
+        assert_eq!(
+            FieldDescriptor::Object(String::new()).as_type(),
+            ValType::I32
+        );
+        assert_eq!(
+            FieldDescriptor::Array(Box::new(FieldDescriptor::Int)).as_type(),
+            ValType::I32
+        );
+    }
+
+    #[test]
+    fn field_descriptor_size() {
+        // Single word (4 byte) types
+        assert_eq!(FieldDescriptor::Boolean.size(), 4);
+        assert_eq!(FieldDescriptor::Byte.size(), 4);
+        assert_eq!(FieldDescriptor::Char.size(), 4);
+        assert_eq!(FieldDescriptor::Short.size(), 4);
+        assert_eq!(FieldDescriptor::Int.size(), 4);
+
+        assert_eq!(FieldDescriptor::Float.size(), 4);
+
+        assert_eq!(FieldDescriptor::Object(String::new()).size(), 4);
+        assert_eq!(
+            FieldDescriptor::Array(Box::new(FieldDescriptor::Int)).size(),
+            4
+        );
+
+        // Double word (8 byte) types
+        assert_eq!(FieldDescriptor::Long.size(), 8);
+        assert_eq!(FieldDescriptor::Double.size(), 8);
+    }
+
+    #[test]
+    fn function_type_with_implicit_this() {
+        let func_type = FunctionType {
+            params: vec![ValType::F32],
+            results: vec![],
+        };
+        let func_type = func_type.with_implicit_this();
+        assert_eq!(func_type.params, [ValType::I32, ValType::F32]);
+        assert_eq!(func_type.results, []);
+    }
+
+    #[test]
+    fn function_type_dispatcher_name() {
+        let func_type = FunctionType {
+            params: vec![ValType::I32, ValType::I64, ValType::F32, ValType::F64],
+            results: vec![ValType::I32],
+        };
+        assert_eq!(func_type.dispatcher_name(), "!Dispatcher_i32i64f32f64_i32");
+        let func_type = FunctionType {
+            params: vec![],
+            results: vec![],
+        };
+        assert_eq!(func_type.dispatcher_name(), "!Dispatcher__");
+    }
+
+    #[test]
+    fn method_descriptor_new() {
+        // Check computes `FunctionType` with return
+        let d = MethodDescriptor::new(
+            vec![FieldDescriptor::Int],
+            ReturnDescriptor::Field(FieldDescriptor::Float),
+        );
+        assert_eq!(d.function_type.params, [ValType::I32]);
+        assert_eq!(d.function_type.results, [ValType::F32]);
+
+        // Check computes `FunctionType` with void return
+        let d = MethodDescriptor::new(vec![], ReturnDescriptor::Void);
+        assert_eq!(d.function_type.params, []);
+        assert_eq!(d.function_type.results, []);
+    }
+
+    #[test]
+    fn method_descriptor_ord() {
+        // Check initially orders by params...
+        let d1 = MethodDescriptor::new(
+            vec![
+                FieldDescriptor::Byte,
+                FieldDescriptor::Object(String::from("A")),
+            ],
+            ReturnDescriptor::Field(FieldDescriptor::Object(String::from("B"))),
+        );
+        let d2 = MethodDescriptor::new(
+            vec![
+                FieldDescriptor::Byte,
+                FieldDescriptor::Object(String::from("B")),
+            ],
+            ReturnDescriptor::Field(FieldDescriptor::Object(String::from("A"))),
+        );
+        assert_eq!(d1.cmp(&d2), Ordering::Less);
+        assert_eq!(d1.cmp(&d1), Ordering::Equal);
+        assert_eq!(d2.cmp(&d1), Ordering::Greater);
+
+        // ...and then by returns if params match
+        let d1 = MethodDescriptor::new(
+            vec![FieldDescriptor::Int],
+            ReturnDescriptor::Field(FieldDescriptor::Object(String::from("B"))),
+        );
+        let d2 = MethodDescriptor::new(
+            vec![FieldDescriptor::Int],
+            ReturnDescriptor::Field(FieldDescriptor::Object(String::from("A"))),
+        );
+        assert_eq!(d1.cmp(&d2), Ordering::Greater);
+        assert_eq!(d2.cmp(&d1), Ordering::Less);
     }
 }
