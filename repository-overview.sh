2f0cac364dd03d928c3eb83409dce00b78198692
MrBBot <me@mrbbot.dev>
2022-05-03 19:35:10 +0100

    Add `repository-overview.sh` script for counting files/lines

---
+++ repository-overview.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -e
+
+# Counts number of files/lines for each entry of the "Repository Overview" using `cloc`
+
+cloc ./benchmarks/{plots,scripts,Makefile} ./src ./Cargo.toml --json | jq -c '{ files: .SUM.nFiles, lines: [.SUM.code, .SUM.comment] | add }'
+
+printf 'benchmarks: '
+cloc ./benchmarks/{plots,scripts,Makefile} --json | jq -c '{ files: .SUM.nFiles, lines: [.SUM.code, .SUM.comment] | add }'
+
+ENTRIES=(
+  "./benchmarks/plots"
+  "./benchmarks/scripts"
+  "./benchmarks/Makefile"
+  "./src"
+  "./src/class"
+  "./src/class/descriptors"
+  "./src/function"
+  "./src/function/structure"
+  "./src/function/locals.rs"
+  "./src/function/visitor.rs"
+  "./src/graph"
+  "./src/output"
+  "./src/output/builtin"
+  "./src/virtuals"
+  "./src/main.rs"
+  "./src/options.rs"
+  "./src/scheduler.rs"
+  "./Cargo.toml"
+)
+for ENTRY in "${ENTRIES[@]}" ; do
+    printf "%s: " "$ENTRY"
+    cloc "$ENTRY" --json | jq -c '{ files: .SUM.nFiles, lines: [.SUM.code, .SUM.comment] | add }'
+done
